0eef559ddba4a1d2e6344d2fff68c703d2892700
JannatMollah <jannatmollah@gmail.com>
2025-10-09 03:26:06 +0600

    Error404 Page

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import ErrorImage from '../../assets/error-404.png'
+import Navber from '../../components/Header/Navber';
+import Footer from '../../components/Footer/Footer';
+import CopyrightFooter from '../../components/Footer/CopyrightFooter';
 
 const ErrorPage = () => {
     return (
         <div>
-            <h1>Page Not Found</h1>
+            <Navber></Navber>
+            <div className='flex flex-col items-center bg-[#E9E9E9] h-screen'>
+                <img src={ErrorImage} className='py-10'/>
+                <h2 className='text-5xl font-semibold'>Oops, page not found!</h2>
+                <p className='text-gray-500 py-5'>The page you are looking for is not available.</p>
+                <a href="" className='btn bg-linear-to-r from-[#632EE3] to-[#9F62F2] text-white'>Go Back!</a>
+            </div>
+            <Footer></Footer>
+            <CopyrightFooter></CopyrightFooter>
         </div>
     );
 };
